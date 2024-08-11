836ca17d3619203e0773b3adc3f901b197e11f0a
muneeb-250 <muneeburrehman250@gmail.com>
2024-08-11 10:59:04 +0500

    basic store, as for now

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,74 @@
 import './App.css'
-
+import { useStore } from './store/store'
 function App() {
+
+  const chars = useStore((store) => store.characters)
+  const deleteChar = useStore((store) => store.deleteCharacter)
+  
+  const btnStyles = {
+    display: 'block',
+    padding: '10px',
+    backgroundColor: 'red',
+    color: 'white',
+    borderRadius: '5px',
+    border: 'none',
+    cursor: 'pointer'
+  }
+
   return (
     <main>
-      <h1>Sustand</h1>
+      <div style={{ textAlign: 'center' }}>
+        <h1>Sustand</h1>
+        <code>A Sus-copy of Zustand</code>
+      </div>
+      <section>
+
+        {
+          chars?.map((char) => (
+            <div key={char.id}>
+              <h2>Name: {char.name}</h2>
+              <p>Age: {char.age}</p>
+              <p>Gender: {char.gender}</p>
+              <p>Rank: {char.rank}</p>
+              <p>Affiliation: {char.affiliation}</p>
+              <div>Abilities: {char.abilities.map(ability => <span> {ability}</span>)}</div>
+              <br />
+              <img src={char.image} alt={char.name} width={500} />
+              <button style={btnStyles} onClick={() => deleteChar!(char.id)}>Delete {char.name}</button>
+            </div>
+          ))
+        }
+
+        <div>
+          Store Structure
+          <pre>
+            {`
+          interface Character {
+            id: string;
+            name: string;
+            age: number;
+            gender: string;
+            rank: string;
+            affiliation: string;
+            abilities: string[];
+            image?: string
+          }
+           
+          interface Store {
+            characters: Character[];
+            addCharacter: (character: Character) => void;
+            deleteCharacter: (id: string) => void;
+            set: (store: Partial<Store>) => void;
+          }
+              `}
+          </pre>
+        </div>
+      </section>
+
+
     </main>
   )
 }
 
 export default App
+
--- src/store/store.tsx
+++ src/store/store.tsx
@@ -9,16 +9,38 @@ export interface Character {
     rank: string;
     affiliation: string;
     abilities: string[];
+    image?: string
 }
 
 export interface Store {
-    characters: string[];
+    characters: Character[];
     addCharacter: (character: Character) => void;
+    deleteCharacter: (id: string) => void;
 
     set: (store: Partial<Store>) => void;
 }
 
 export const useStore = create<Partial<Store>>((set) => ({
-    characters: [],
+    characters: [
+        {
+            id: "1",
+            name: "Eren Jaegar",
+            age: 19,
+            gender: "male",
+            rank: "A+",
+            affiliation: "N/A",
+            abilities: [
+                "titan shifter", "Freedom Fighter"
+            ],
+            image: "https://static1.srcdn.com/wordpress/wp-content/uploads/2022/01/Eren-Jaeger-in-Attack-on-Titan.jpg"
+        }
+    ],
+    deleteCharacter: (id: string) => set((store) => ({
+        characters: store?.characters?.filter((char) => char.id !== id)
+    })),
+    addCharacter: (character: Character) => set((store) => ({
+        characters: store?.characters?.concat({ ...character, id: `${store?.characters?.length + 1}` })
+    })),
+
     set: (store) => set(store),
 }));
